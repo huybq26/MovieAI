3e206b1b4768d30a8ca3373ad2997926609b9b4e
huybq26 <bui.quang.huy.2002@gmail.com>
2024-03-01 23:41:22 +0800

    Added Film-related Entities

--- src/main/java/com/example/movieAI/film_management/TagRepository.java
+++ src/main/java/com/example/movieAI/film_management/TagRepository.java
@@ -3,4 +3,5 @@ package com.example.movieAI.film_management;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TagRepository extends CrudRepository<TagEntity, Long> {
+
 }
--- src/main/java/com/example/movieAI/film_management/TriviaRepository.java
+++ src/main/java/com/example/movieAI/film_management/TriviaRepository.java
@@ -3,4 +3,5 @@ package com.example.movieAI.film_management;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TriviaRepository extends CrudRepository<TriviaEntity, Long> {
+
 }
